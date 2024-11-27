da7bd1ed6603d74bc32972166e55f01eaa425a56
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-11-27 15:00:28 +0100

    ok

--- routes/web.php
+++ routes/web.php
@@ -6,55 +6,52 @@ use App\Http\Controllers\ScenarioController;
 use App\Http\Controllers\StepController;
 use App\Http\Controllers\ResultsController;
 
+// Debug route
 Route::get('/debug', function () {
     return app('App\Http\Controllers\DebugController')->index();
 })->middleware('auth');
 
-Route::resource('scenarios', ScenarioController::class)->middleware('auth');
-
-Route::get('/scenarios/{scenario}/steps/create', [StepController::class, 'create'])->name('steps.create');
-
-Route::get('/results', [ResultsController::class, 'index'])->name('results.index');
-
-Route::get('/results/{scenario}', [ResultsController::class, 'show'])->name('results.show');
-
-Route::post('/scenarios/{scenario}/steps', [StepController::class, 'store'])->name('steps.store');
-
-Route::get('/scenarios/{scenario}/steps/{step}/edit', [StepController::class, 'edit'])->name('steps.edit');
-
-// delete step
-Route::delete('/scenarios/{scenario}/steps/{step}/delete', [StepController::class, 'destroy'])->name('steps.destroy');
-
-// update step
-Route::put('/scenarios/{scenario}/steps/{step}/edit', [StepController::class, 'update'])->name('steps.update');
-
-// update scenario
-Route::put('/scenarios/{scenario}/edit', [ScenarioController::class, 'update'])->name('scenarios.update');
-
-// get scenario by slug
-Route::get('/scenarios/start/{slug}', [ScenarioController::class, 'showBySlug']);
-
-// verify the scenario access code
-Route::post('/scenarios/start/{slug}', [ScenarioController::class, 'verifyAccessCode'])->name('verifyAccessCode');
-
-
-Route::post('/scenario/{scenario}/update-step-order', [ScenarioController::class, 'updateStepOrder'])
-    ->name('scenario.update-step-order');
-
-
-// post to results controller to store the results
-Route::post('/results', [ResultsController::class, 'store'])->name('results.store');
-
+// Home route
 Route::get('/', function () {
-   // get the debug controller index method
     return app('App\Http\Controllers\ScenarioController')->index();
 })->middleware('auth');
 
+// Authenticated routes
+Route::middleware(['auth'])->group(function () {
+    // Scenarios
+    Route::resource('scenarios', ScenarioController::class);
+    Route::put('/scenarios/{scenario}/edit', [ScenarioController::class, 'update'])->name('scenarios.update');
+    Route::post('/scenario/{scenario}/update-step-order', [ScenarioController::class, 'updateStepOrder'])
+        ->name('scenario.update-step-order');
 
-Route::group(['middleware' => ['guest']], function() {
-  Route::get('login', [AuthController::class, 'showLogin'])->name('login.show');
-  Route::post('login', [AuthController::class, 'login'])->name('login');
-  Route::get('verify-login/{token}', [AuthController::class, 'verifyLogin'])->name('verify-login');
+    // Steps
+    Route::get('/scenarios/{scenario}/steps/create', [StepController::class, 'create'])->name('steps.create');
+    Route::post('/scenarios/{scenario}/steps', [StepController::class, 'store'])->name('steps.store');
+    Route::get('/scenarios/{scenario}/steps/{step}/edit', [StepController::class, 'edit'])->name('steps.edit');
+    Route::put('/scenarios/{scenario}/steps/{step}/edit', [StepController::class, 'update'])->name('steps.update');
+    Route::delete('/scenarios/{scenario}/steps/{step}/delete', [StepController::class, 'destroy'])->name('steps.destroy');
+
+    // Results
+    Route::get('/results', [ResultsController::class, 'index'])->name('results.index');
+    Route::get('/results/{scenario}', [ResultsController::class, 'show'])->name('results.show');
 });
 
+// Public routes
+Route::post('/results', [ResultsController::class, 'store'])->name('results.store');
+
+// Guest routes
+Route::middleware(['guest'])->group(function () {
+   
+
+    // Authentication
+    Route::get('login', [AuthController::class, 'showLogin'])->name('login.show');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::get('verify-login/{token}', [AuthController::class, 'verifyLogin'])->name('verify-login');
+});
+
+ // Public scenario access
+ Route::get('/scenarios/start/{slug}', [ScenarioController::class, 'showBySlug']);
+ Route::post('/scenarios/start/{slug}', [ScenarioController::class, 'verifyAccessCode'])->name('verifyAccessCode');
+
+// Logout route (available to all)
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
