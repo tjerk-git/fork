9443ac33319c27a803e25c48d4427a73246afb63
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-12-10 18:46:40 +0100

    ok

--- resources/views/emails/magic-login-link.blade.php
+++ resources/views/emails/magic-login-link.blade.php
@@ -1,6 +1,12 @@
 @component('mail::message')
 Inloggen kan je via deze link:
+
 @component('mail::button', ['url' => $url])
 Inloggen
 @endcomponent
+
+Als de knop niet werkt, kan je ook deze link gebruiken: <br>
+{{ $url }}
+
+Deze link is verzonden op {{ now()->format('d-m-Y') }} om {{ now()->format('H:i') }}.
 @endcomponent
